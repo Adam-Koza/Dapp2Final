52ce36abe7ea37307c7d3bcbba26a3fe92408a36
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-04-18 20:27:51 -0400

    Finish q9.

---
+++ q1/src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.receivePosts = this.receivePosts.bind(this);
+  //   this.fetchPosts = this.fetchPosts.bind(this);
+  // }
+
+  // fetchPosts = async () => {
+  //   return dispatch => {
+  //     return fetch(`https://gist.githubusercontent.com/thomasmetta/4f86937797f89cc74547d985810dd5b6/raw/374f7ae4b9b9817f74f534b27e97c17a73eb6b4f/random.json`)
+  //       .then(response => response.json())
+  //       .then(json => dispatch(receivePosts(json)))
+  //   }
+  // };
+
+  // receivePosts = async (json) => {
+  //   return json.filter((item) => {
+  //     console.log(item);
+  //     return item.isActive == true;
+  //   })
+
+  // };
+
+  
+
+  render() {
+    async function fetchPosts () {
+      return dispatch => {
+        return fetch(`https://gist.githubusercontent.com/thomasmetta/4f86937797f89cc74547d985810dd5b6/raw/374f7ae4b9b9817f74f534b27e97c17a73eb6b4f/random.json`)
+          .then(response => response.json())
+          .then(json => dispatch(receivePosts(json)))
+      }
+    };
+    function receivePosts (json) {
+      return json.filter((item) => {
+        console.log(item);
+        return item.isActive === true;
+      })
+  
+    };
+
+
+    fetchPosts();
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ q9/q9.js
@@ -0,0 +1,6 @@
+let signveri = require('eth-sign-verify');
+
+signveri.signMessage("Test Message", "0x0123456789012345678901234567890123456789012345678901234567890123").then(sig => {
+    console.log(sig);
+    return signveri.verifyMessage("Test Message", sig, "0x14791697260E4c9A71f18484C9f997B308e59325")
+}).then(console.log);
